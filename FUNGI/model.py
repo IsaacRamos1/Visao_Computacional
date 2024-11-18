d365da63239f2c33b13732bf8e3c2a011bb6fd39
IsaacRamos1 <isac_ramos1@ufpi.edu.br>
2024-11-18 12:59:32 -0300

    fixed_custom_model

--- FUNGI/model.py
+++ FUNGI/model.py
@@ -1,23 +1,28 @@
 import torch.nn as nn
+import torch
 import torchvision.models as models
 
 class MyModel(nn.Module):
-    def __init__(self, in_channels=3, out_channels=1):
+    def __init__(self, in_channels=3, out_channels=5):
         super(MyModel, self).__init__()
+        self._out_channels = out_channels
 
         self.encoder1 = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=1)    # outra opção avgpooling2D
         )
-                                                            ## revisar e consertar erros para MyModel
-                                                            ## conflito de tipos de variável crit(output, label)
+
+        self.flatten = nn.Flatten()
+
     def forward(self, x):
         x = self.encoder1(x)
         x = self.flatten(x)
-        output = self.classifier(x)
-        return output
+        
+        self.dense = nn.Linear(x.shape[1], self._out_channels).to('cuda:0')
+        x = self.dense(x)
+        return x
     
 
 class MyPreTrainedModel(nn.Module):
--- main.py
+++ main.py
@@ -17,8 +17,6 @@ if __name__ == '__main__':
     val_dataloader = dataloader.get_val_dataloader()
     test_dataloader = dataloader.get_test_dataloader()
 
-    #classe_idx = {'BASH': 0, 'BBH': 1, 'GMA': 2, 'SHC': 3, 'TSH': 4}
-
     #model = MyModel(out_channels=5).to('cuda:0')
     model = MyPreTrainedModel(out_channels=5).to('cuda:0')
     optim = optim.Adam(model.parameters(), lr=0.001)
@@ -36,31 +34,13 @@ if __name__ == '__main__':
             label = label.to('cuda:0')
             optim.zero_grad()
             output = model(image)
-
-            #print(output.shape)
-            #print('-------------')
-            #print(label.shape)
-            #exit()
             loss = crit(output, label)
             loss.backward()
             optim.step()
             run_loss.append(loss.item())
             train_samples.set_description(f'Epoca {epoca+1}/{epocas}, loss: {np.mean(run_loss):0.6f}')
-            
             model.eval()
             val_samples = tqdm(val_dataloader)
             for image, label in val_samples:
                 pass
 
-
-
-
-
-#for image, label in train_dataloader:
-#            for batch in range(batch_size):
-#                image = image.detach().cpu().numpy()[batch].transpose(1, 2, 0)
-#                print(f'Class label: {label[batch]}')
-#                cv2.imshow('image', image)
-
-#                if cv2.waitKey(0) == ord('q'):
-#                    break
